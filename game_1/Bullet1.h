37fbc720f09a1884f7c1ab17d8ad13158fd517b9
Maple236 <drzx0581@leeds.ac.uk>
2026-04-29 22:18:07 +0800

    Add files via upload

---
+++ game_1/Bullet1.h
@@ -0,0 +1,11 @@
+#ifndef BULLET1_H
+#define BULLET1_H
+
+//Bullet
+typedef struct {
+    int16_t x, y;
+    int8_t dir; // 1 is right, -1 is left
+    int8_t active; // 1 is flying, 0 is not
+} Bullet_t;
+
+#endif
